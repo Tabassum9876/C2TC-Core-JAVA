7a48458a53dbd6a50d02a08ad69dc6b3a41c758c
Tabassum9876 <tabassumbanu237@gmail.com>
2022-04-06 23:31:44 +0530

    first commit

---
+++ PackageDemo/src/com/tns/mypackage/MyClass.java
@@ -0,0 +1,11 @@
+package com.tns.mypackage;
+
+public class MyClass {
+
+	public void getNames(String s)
+	{
+	System.out.println(s);	
+
+	}
+
+}
---
+++ PackageDemo/src/com/tns/psvm/PrintName.java
@@ -0,0 +1,15 @@
+package com.tns.psvm;
+
+import com.tns.mypackage.MyClass;
+
+public class PrintName {
+
+   public static void main(String[] args) {
+	   
+	   String name = "Welcome to GULBARGA";
+		MyClass obj = new MyClass();
+		obj.getNames(name);
+
+	}
+
+}
